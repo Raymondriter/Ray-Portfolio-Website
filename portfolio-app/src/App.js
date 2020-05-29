258677c46529584e714075c4655890409f2fe774
Raymondriter <rpr2998@gmail.com>
2020-05-28 21:00:22 -0400

    adding footer

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -5,6 +5,8 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
+import Footer from "./components/Footer";
+
 class App extends React.Component {
 
     constructor(props) {
@@ -35,18 +37,19 @@ class App extends React.Component {
         return (
             <Router>
                 <Container className="p-0" fluid={true}>
+                    <Navbar className="border-bottom" bg="transparent" expand="lg">
+                        <Navbar.Brand>Raymond Riter</Navbar.Brand>
+                        <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+                        <Navbar.Collapse id="navbar-toggle">
+                            <Nav className="ml-auto">
+                                <Link className="nav-link" to="/">Home</Link>
+                                <Link className="nav-link" to="/about">About</Link>
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    <Footer />
                 </Container>
-                <Navbar className="border-bottom" bg="transparent" expand="lg">
-                    <Navbar.Brand>Raymond Riter</Navbar.Brand>
-                    <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-                    <Navbar.Collapse id="navbar-toggle">
-                        <Nav className="ml-auto">
-                          <Link className="nav-link" to="/">Home</Link>
-                          <Link className="nav-link" to="/about">About</Link>
-                          <Link className="nav-link" to="/contact">Contact</Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
             </Router>
         );
     }
