06eaa98c5d96060450c8209dde151c4a506c8c6a
Raymondriter <rpr2998@gmail.com>
2020-05-28 20:36:49 -0400

    header added with working pages for home about and contact

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,38 +1,55 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'Raymond Riter',
-      headerLinks: [
-        { title: 'Home', path: '/' },
-        { title: 'About', path: '/about' },
-        { title: 'Contact', path: '/contact' },
-      ],
-      home: {
-        title: 'Aspiring in, Development/Cyber Security',
-        subTitle: 'Projects, accomplishments that make a difference',
-        text: 'Check my techistory below'
-      },
-      about: {
-        title: 'About Me',
-        subTitle: 'Projects, accomplishments that make a difference',
-        text: 'Check my techistory below'
-      },
-      contact: {
-        title: 'Let\'s Talk!'
-      }
-    }
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Raymond Riter',
+            headerLinks: [
+                {title: 'Home', path: '/'},
+                {title: 'About', path: '/about'},
+                {title: 'Contact', path: '/contact'},
+            ],
+            home: {
+                title: 'Aspiring in, Development/Cyber Security',
+                subTitle: 'Projects, accomplishments that make a difference',
+                text: 'Check my techistory below'
+            },
+            about: {
+                title: 'About Me',
 
-  render() {
-    return (
-        <div>Hello from React</div>
-    );
-  }
+            },
+            contact: {
+                title: 'Let\'s Talk!'
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Container className="p-0" fluid={true}>
+                </Container>
+                <Navbar className="border-bottom" bg="transparent" expand="lg">
+                    <Navbar.Brand>Raymond Riter</Navbar.Brand>
+                    <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+                    <Navbar.Collapse id="navbar-toggle">
+                        <Nav className="ml-auto">
+                          <Link className="nav-link" to="/">Home</Link>
+                          <Link className="nav-link" to="/about">About</Link>
+                          <Link className="nav-link" to="/contact">Contact</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
 
 }
 
